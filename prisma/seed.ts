a3a222e9cb719ab3e28e410d4e462931686ae3ea
Andrew Kononenko <wastardy.k@gmail.com>
2025-05-25 22:12:47 +0300

    feat(prisma): add prisma client, initial schema and seed file

---
+++ prisma/seed.ts
@@ -0,0 +1,13 @@
+import { prisma } from "./client";
+
+async function main() {}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (err) => {
+    console.error(err);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
