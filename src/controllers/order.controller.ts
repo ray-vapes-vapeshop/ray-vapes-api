b945ab8a4c9458da53356cbc3376984cf31f15ec
Andrew Kononenko <wastardy.k@gmail.com>
2025-06-07 01:15:52 +0300

    fix(order): update getOrdersByFilter to use mapOrdersQueryToDto for improved query handling

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -3,14 +3,14 @@ import { Request, Response } from "express";
 import { OrderService } from "../services/order.service";
 import { OrdersQueryDto } from "../dtos/orders-query.dto";
 import { SuccessResponseDto } from "../dtos/success-response.dto";
-import { OrdersQuerySchema } from "../validators/orders-query.validator";
 import { CreateOrderDto } from "../dtos/create-order.dto";
+import { mapOrdersQueryToDto } from "../mappers/order-query.mapper";
 
 export class OrderController {
   constructor(private readonly orderService: OrderService = new OrderService()) {}
 
   async getOrdersByFilter(req: Request, res: Response) {
-    const ordersQuery: OrdersQueryDto = OrdersQuerySchema.parse(req.query);
+    const ordersQuery: OrdersQueryDto = mapOrdersQueryToDto(req.body);
 
     const orders = await this.orderService.getOrdersByFilter(ordersQuery);
 
