073b488d35b3c59305636e36a16b4ac9415d0357
Andrew Kononenko <wastardy.k@gmail.com>
2025-06-07 01:28:14 +0300

    fix(order): update getOrdersByFilter to use OrdersQuerySchema for query validation and mapping

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -5,14 +5,17 @@ import { OrdersQueryDto } from "../dtos/orders-query.dto";
 import { SuccessResponseDto } from "../dtos/success-response.dto";
 import { CreateOrderDto } from "../dtos/create-order.dto";
 import { mapOrdersQueryToDto } from "../mappers/order-query.mapper";
+import { OrdersQueryType } from "../types/orders-query.type";
+import { OrdersQuerySchema } from "../validators/orders-query.validator";
 
 export class OrderController {
   constructor(private readonly orderService: OrderService = new OrderService()) {}
 
   async getOrdersByFilter(req: Request, res: Response) {
-    const ordersQuery: OrdersQueryDto = mapOrdersQueryToDto(req.body);
+    const ordersQuery: OrdersQueryType = OrdersQuerySchema.parse(req.query);
+    const orderQueryDto: OrdersQueryDto = mapOrdersQueryToDto(ordersQuery);
 
-    const orders = await this.orderService.getOrdersByFilter(ordersQuery);
+    const orders = await this.orderService.getOrdersByFilter(orderQueryDto);
 
     res.json(new SuccessResponseDto(orders));
   }
