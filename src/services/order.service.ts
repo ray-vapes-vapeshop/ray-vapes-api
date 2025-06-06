d7bdd385b50bc9dc825e86a0ec72b0707fe7bf22
Andrew Kononenko <wastardy.k@gmail.com>
2025-06-06 16:51:06 +0300

    feat(order): implement OrderService with methods for order retrieval by id and order creation

---
+++ src/services/order.service.ts
@@ -0,0 +1,48 @@
+import { NotFound } from "http-errors";
+
+import { CreateOrderDto } from "../dtos/create-order.dto";
+import { OrdersQueryDto } from "../dtos/orders-query.dto";
+import { OrderRepository } from "../repositories/order.repository";
+import errorConstant from "../constants/error.constant";
+import { ProductsRepository } from "../repositories/products.repository";
+
+export class OrderService {
+  constructor(
+    private readonly orderRepository: OrderRepository = new OrderRepository(),
+    private readonly productsRepository: ProductsRepository = new ProductsRepository(),
+  ) {}
+
+  async getOrdersByFilter(ordersQuery: OrdersQueryDto) {
+    console.log(ordersQuery);
+  }
+
+  async getOrderById(orderId: string) {
+    return await this.orderRepository.getOrderById(orderId);
+  }
+
+  async getOrderByIdOrThrow(orderId: string) {
+    const order = await this.getOrderById(orderId);
+
+    if (!order) {
+      throw new NotFound(errorConstant.ORDER_NOT_FOUND);
+    }
+
+    return order;
+  }
+
+  async getOrderWithRelationsById(orderId: string) {
+    return await this.orderRepository.getOrderWithRelationsById(orderId);
+  }
+
+  async createOrder(createOrderDto: CreateOrderDto) {
+    const productIds = [...new Set(createOrderDto.items.map((item) => item.productId))];
+
+    const existingProducts = await this.productsRepository.getProductsByIds(productIds);
+
+    if (existingProducts.length !== productIds.length) {
+      throw new NotFound(errorConstant.PRODUCTS_NOT_FOUND);
+    }
+
+    return await this.orderRepository.createOrder(createOrderDto);
+  }
+}
