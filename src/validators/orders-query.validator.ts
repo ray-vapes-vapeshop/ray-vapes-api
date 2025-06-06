a72f6f523c69d56648abb13e86ef0455b7e33471
Andrew Kononenko <wastardy.k@gmail.com>
2025-06-07 01:17:01 +0300

    refactor(validator): simplify date validation in OrdersQuerySchema by removing custom refinements

--- src/validators/orders-query.validator.ts
+++ src/validators/orders-query.validator.ts
@@ -2,33 +2,15 @@ import { z as zod } from "zod";
 
 import { PageQuerySchema } from "./page-query.validator";
 import { OrderStatus } from "@prisma/client";
-import validationConstant from "../constants/validation.constant";
 
 export const OrdersQuerySchema = PageQuerySchema.merge(
   zod.object({
     search: zod.string().optional(),
+    location: zod.string().optional(),
     orderStatus: zod.nativeEnum(OrderStatus).optional(),
     minPriceCents: zod.coerce.number().optional(),
     maxPriceCents: zod.coerce.number().optional(),
-    dateFrom: zod
-      .string()
-      .optional()
-      .refine(
-        (val) => {
-          if (!val) return true;
-          return !isNaN(Date.parse(val));
-        },
-        { message: validationConstant.INVALID_START_DATE },
-      ),
-    dateTo: zod
-      .string()
-      .optional()
-      .refine(
-        (val) => {
-          if (!val) return true;
-          return !isNaN(Date.parse(val));
-        },
-        { message: validationConstant.INVALID_END_DATE },
-      ),
+    dateFrom: zod.string().optional(),
+    dateTo: zod.string().optional(),
   }),
 );
